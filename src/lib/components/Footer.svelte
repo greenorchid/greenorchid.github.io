e6628c8f3aa584c14246462a9890c140dc472105
greenorchid <30530138+greenorchid@users.noreply.github.com>
2026-02-01 12:27:48 +0000

    Update Footer.svelte

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,12 +5,12 @@
 <footer
 	class="mt-auto flex flex-col gap-2 border-t p-4 text-sm text-gray-500 sm:flex-row sm:items-center sm:justify-between"
 >
-	<div class="order-1 sm:order-2">
+	<div class="order-2 sm:order-1">
 		© 2026 Marc P. Behan | Version: {import.meta.env.DEV
 			? 'local-dev'
 			: import.meta.env.VITE_APP_VERSION}
 	</div>
-	<div class="order-2 sm:order-1">
+	<div class="order-1 sm:order-2">
 		<a href={resolve('/disclaimer')} rel="noopener noreferrer"> Disclaimer </a>
 	</div>
 </footer>

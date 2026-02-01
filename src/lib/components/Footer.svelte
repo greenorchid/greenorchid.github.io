830010a3ee40ff18cb3098c8f3a248b0177a55aa
greenorchid <30530138+greenorchid@users.noreply.github.com>
2026-02-01 12:15:38 +0000

    Update Footer.svelte add ordering to div

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,12 +5,12 @@
 <footer
 	class="mt-auto flex flex-col gap-2 border-t p-4 text-sm text-gray-500 sm:flex-row sm:items-center sm:justify-between"
 >
-	<div>
+	<div class="order-1 sm:order-2">
 		© 2026 Marc P. Behan | Version: {import.meta.env.DEV
 			? 'local-dev'
 			: import.meta.env.VITE_APP_VERSION}
 	</div>
-	<div>
+	<div class="order-2 sm:order-1">
 		<a href={resolve('/disclaimer')} rel="noopener noreferrer"> Disclaimer </a>
 	</div>
 </footer>
